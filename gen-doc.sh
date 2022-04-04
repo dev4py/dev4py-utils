f34b27b9363779fa25382bf9b8c664d0014f628b
St4rG00se <st4rg00se@protonmail.com>
2022-04-04 10:23:03 +0200

    docs: update docs template

--- gen-doc.sh
+++ gen-doc.sh
@@ -2,6 +2,7 @@
 
 ./project.py run \
   -m pdoc \
+  -t ./docs/templates \
   -d google \
   --logo "https://avatars.githubusercontent.com/u/100195108?s=70&v=4" \
   --logo-link "https://github.com/dev4py" \
